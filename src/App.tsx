79dbbc0b25f319967a44f6b88f2ebbdd7bf7667b
Sajjad Abedi <sajadxabedi@gmail.com>
2024-11-14 18:00:40 +0200

    fix: removed the styles and updated the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!♥︎ </h1>
+      <div className="h-[100vh] w-full flex justify-center items-center bg-gray-50 dark:bg-neutral-900">
+        <h1 className="text-3xl font-bold underline">Hello world!♥︎ </h1>
+      </div>
     </>
   )
 }
