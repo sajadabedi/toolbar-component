608e06e7df7de86e631ee607895019902903ff6d
Sajjad Abedi <sajadxabedi@gmail.com>
2024-11-19 16:55:28 +0200

    Collaborators tab

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import {
   Rabbit,
   Share2,
   ShieldCheck,
-  ShieldIcon,
 } from 'lucide-react'
 import img1 from './assets/1.png'
 import img2 from './assets/2.png'
@@ -63,17 +62,100 @@ function App() {
           tooltip="Jakub, Jonas and 1 more"
         />
         <Toolbar.Content>
-          <p className="text-sm">Collaborators</p>
+          <ul className="flex flex-col gap-1 w-full">
+            {img1 && (
+              <li className="flex gap-2 items-center">
+                <img
+                  src={img1}
+                  alt="Collaborator 1"
+                  className="size-6 rounded-full"
+                />
+                <div className="flex flex-col">
+                  <span className="text-sm">Jakub</span>
+                  <span className="text-xs text-gray-500">
+                    Last edited: 10 minutes ago
+                  </span>
+                </div>
+              </li>
+            )}
+            {img2 && (
+              <li className="flex gap-2 items-center">
+                <img
+                  src={img2}
+                  alt="Collaborator 2"
+                  className="size-6 rounded-full"
+                />
+                <div className="flex flex-col">
+                  <span className="text-sm">Jonas</span>
+                  <span className="text-xs text-gray-500">
+                    Last edited: 1 hour ago
+                  </span>
+                </div>
+              </li>
+            )}
+            {img3 && (
+              <li className="flex gap-2 items-center">
+                <img
+                  src={img3}
+                  alt="Collaborator 3"
+                  className="size-6 rounded-full"
+                />
+                <div className="flex flex-col">
+                  <span className="text-sm">Kuba</span>
+                  <span className="text-xs text-gray-500">
+                    Last edited: 2 hours ago
+                  </span>
+                </div>
+              </li>
+            )}
+            <button className="mt-2 text-sm text-gray-700 dark:text-neutral-400 shadow-subtle rounded-md text-[13px] w-full text-center bg-white px-2 py-1 dark:bg-neutral-800">
+              View all →
+            </button>
+          </ul>
         </Toolbar.Content>
       </Toolbar.Item>
 
       <Toolbar.Item>
         <Toolbar.Trigger
           icon={<Rabbit className="size-5" />}
-          tooltip="Rabbit"
+          tooltip="Improve performance"
         />
         <Toolbar.Content>
-          <p className="text-sm">Settings</p>
+          <div className="flex flex-col gap-1 w-full">
+            <ul className="flex flex-wrap gap-1">
+              <li className="rounded-full text-[13px] flex border items-center gap-1 min-w-9 text-center border-gray-200 dark:border-neutral-700 text-gray-700 dark:text-neutral-200 px-2 py-[1px] bg-gray-100 dark:bg-neutral-700">
+                All
+              </li>
+              <li className="rounded-full text-[13px] flex border items-center gap-1 min-w-9 text-center border-gray-200 dark:border-neutral-700 text-gray-700 dark:text-neutral-200 px-2 py-[1px] bg-white dark:bg-neutral-800">
+                <span className="size-2 rounded-full block bg-yellow-500" />
+                0.10
+              </li>
+              <li className="rounded-full text-[13px] flex border items-center gap-1 min-w-9 text-center border-gray-200 dark:border-neutral-700 text-gray-700 dark:text-neutral-200 px-2 py-[1px] bg-white dark:bg-neutral-800">
+                <span className="size-2 rounded-full block bg-red-500" />
+                0.24
+              </li>
+            </ul>
+            <ul className="flex flex-wrap gap-2 mt-1">
+              <li className="shadow-subtle flex flex-col rounded-md text-[13px] w-full bg-white px-3 py-2 dark:bg-neutral-800 items-baseline">
+                <h4 className="text-sm">
+                  <span className="size-2 rounded-full inline-block border-2 border-red-500 mr-1" />
+                  Image compression
+                </h4>
+                <p className="text-[13px] text-gray-500 dark:text-neutral-400">
+                  Compress your images to reduce the size of your website.
+                </p>
+              </li>
+              <li className="shadow-subtle flex flex-col rounded-md text-[13px] w-full bg-white px-3 py-2 dark:bg-neutral-800 items-baseline">
+                <h4 className="text-sm">
+                  <span className="size-2 rounded-full inline-block border-2 border-yellow-500 mr-1" />
+                  Code splitting
+                </h4>
+                <p className="text-[13px] text-gray-500 dark:text-neutral-400">
+                  Split your code into smaller chunks.
+                </p>
+              </li>
+            </ul>
+          </div>
         </Toolbar.Content>
       </Toolbar.Item>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ export default {
       boxShadow: {
         elevated:
           '0 0 0 1px rgb(0 0 0 / 0.05), 0 4px 8px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)',
+        subtle: '0 0 0 1px rgb(0 0 0 / 0.05), 0 1px 4px rgb(0 0 0 / 0.1)',
         elevatedDark:
           '0 0 0 1px rgb(255 255 255 / 0.2), 0 4px 8px -1px rgb(0 0 0 / 0.2), 0 2px 4px -2px rgb(0 0 0 / 0.2)',
       },
